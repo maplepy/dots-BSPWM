11a3c8bd1b4e815380393bd118e7489c3f7490bb
Maplepy <github.register@slmail.me>
2022-09-11 04:46:41 +0200

    Move install script

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#! /usr/bin sh
+
+# Install yay
+sudo pacman -S git base-devel
+cd
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install fish shell
+yay -S fish fisher
+fisher install franciscolourenco/done
+fisher install gazorby/fish-abbreviation-tips
+fisher install jorgebucaran/autopair.fish
+fisher install andreiborisov/sponge
+fisher install nickeb96/puffer-fish
+# fisher install pure-fish/pure or
+fisher install IlanCosman/tide@v5
