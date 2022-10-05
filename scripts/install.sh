513f79fd0fe678e1b453771aaabe622a00b8387f
Maplepy <github.register@slmail.me>
2022-10-05 23:55:13 +0200

    Change fish plugins install and dotfiles install

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,8 +11,6 @@ cd
 # Install dotfiles
 cd
 read -rep "Dotfiles folder name: " -i ".dotfiles" folder
-# read -rp "Git repo: " -i "https://github.com/maplepy/dots-BSPWM" repo
-read -rp "Git repo: " -i "git@github.com:maplepy/dots-BSPWM.git" repo
 dots_cmd="/usr/bin/git --git-dir=$HOME/$folder/ --work-tree=$HOME"
 
 echo "Initialising dotfiles folder to $HOME/$folder/"
@@ -20,7 +18,7 @@ printf "\nAdding exception for git\n"
 echo "$folder" >> "$HOME/.gitignore"
 
 printf "Cloning the dotfiles repo\n"
-git clone --bare "$repo" "$HOME/$folder"
+git clone --bare "https://github.com/maplepy/dots-BSPWM" "$HOME/$folder"
 $dots_cmd config --local status.showUntrackedFiles no
 
 printf "\nSuccessfully initialised the dotfiles repo\n"
@@ -40,13 +38,14 @@ chsh -s /bin/fish
 sudo chsh -s /bin/fish
 
 # Install fish plugins
-fisher install franciscolourenco/done
-fisher install gazorby/fish-abbreviation-tips
-fisher install jorgebucaran/autopair.fish
-fisher install andreiborisov/sponge
-fisher install nickeb96/puffer-fish
-# fisher install pure-fish/pure or
-fisher install IlanCosman/tide@v5
+fisher update
+# fisher install franciscolourenco/done
+# fisher install gazorby/fish-abbreviation-tips
+# fisher install jorgebucaran/autopair.fish
+# fisher install andreiborisov/sponge
+# fisher install nickeb96/puffer-fish
+# # fisher install pure-fish/pure or
+# fisher install IlanCosman/tide@v5
 
 # Install firewall
 sudo ufw limit 22/tcp
@@ -65,8 +64,7 @@ if [[ "$git_ask" == "y" ]]; then
     ssh-keygen -t ed25519 -C "$github_mail"
 
     xdg-open "https://github.com/settings/keys"
-    $dots_cmd remote set-url origin git@github.com:maplepy/dots-bspwm
-    .git
+    $dots_cmd remote set-url origin git@github.com:maplepy/dots-bspwm.git
 fi
 
 # Auto update mirrors (reflector)
@@ -75,8 +73,8 @@ sudo ln ~/.config/etc/reflector.conf /etc/xdg/reflector/reflector.conf
 sudo systemctl enable --now reflector.service
 
 # Disable lightdm and use ly
-sudo systemctl disable --now lightdm
-sudo systemctl enable --now ly
+# sudo systemctl disable --now lightdm
+sudo systemctl enable ly
 
 # Reload BSPWM and notify user
-bspc wm -r && notify-send Installation "Installation should be complete now"
+bspc wm -r && notify-send -u critical Installation "Installation should be complete now\nPlease reboot when you're ready"
