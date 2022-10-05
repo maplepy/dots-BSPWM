0b69db1df69dff12710fefc02d6595ece75dc50d
Maplepy <github.register@slmail.me>
2022-10-05 16:51:24 +0200

    Add ufw (firewall) and a bare config in the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,9 +20,15 @@ fisher install nickeb96/puffer-fish
 # fisher install pure-fish/pure or
 fisher install IlanCosman/tide@v5
 
+# Install firewall
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+sudo systemctl enable --now ufw.service
 
-### Enable services
-
-# Auto update mirrors
+# Auto update mirrors (reflector)
 ln ~/.config/etc/reflector.conf /etc/xdg/reflector/reflector.conf
 sudo systemctl enable --now reflector.service
