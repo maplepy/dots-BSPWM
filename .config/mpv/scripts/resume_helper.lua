3920346cbbf3533f58c088e58a38c0e0c461aeb5
Maplepy <github.register@slmail.me>
2022-10-21 04:14:08 +0200

    Change threshold for completed videos to 90%

--- .config/mpv/scripts/resume_helper.lua
+++ .config/mpv/scripts/resume_helper.lua
@@ -2,11 +2,11 @@
 --
 -- watch-later saving at the very end of the file be annoying when you want to
 -- play that file again.   This script always starts a file back at the
--- beginning if it was previously >95% complete.
+-- beginning if it was previously >90% complete.
 local mp = require 'mp'
 
 mp.register_event("file-loaded", function()
-    if mp.get_property_native("percent-pos") > 95 then
+    if mp.get_property_native("percent-pos") > 90 then
         mp.commandv("seek", 0.0, "absolute")
     end
 end)
