8d2d20847e82141aa861aa3fba7be456268ac564
Maplepy <github.register@slmail.me>
2022-12-13 20:34:28 +0100

    sort aliases and add git related aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,46 +2,51 @@
 set fish_greeting
 
 #
-## Quick aliases
+## ALIASES
 #
 
+# Quick aliases
 alias v='nvim'
+alias df='df -h'
+#alias free='free -m'
 
-# fancy ls
-alias ls='lsd --group-dirs first'
-alias l='ls -lv'
-alias la='ls -A'
-alias ll='ls -lA'
+# Ls
+alias ls='lsd --group-dirs first' # shows directories first
+alias l='ls -l' # detailed ls
+alias la='ls -A' # ls with hidden files
+alias ll='ls -lA' # detailed ls with hidden files
 alias lt='ls --tree'
 
-alias df='df -h'
-alias free='free -m'
-
-alias ..='cd ..'
-alias ...='cd ../..'
-
-alias grub-regen='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+# Pacman / yay
 alias unlock='sudo rm /var/lib/pacman/db.lck'
 alias yayes='yay -Syu --sudoloop --noconfirm'
-alias pacman-orphaned='sudo pacman -Rns (pacman -Qtdq)' # Remove orphaned packages
-alias pacman-purge='yay -Sc && yay -c' # Clear cache and unused packages
-alias mirror-update='curl -s "https://archlinux.org/mirrorlist/?country=FR&country=GB&country=DE&protocol=https&use_mirror_status=on" | sed -e "s/^#Server/Server/" -e "/^#/d" | rankmirrors -n 5 -'
+alias pacman_orphaned='sudo pacman -Rns (pacman -Qtdq)' # Remove orphaned packages
+alias pacman_purge='yay -Sc && yay -c' # Clear cache and unused packages
+alias mirror_update='curl -s "https://archlinux.org/mirrorlist/?country=FR&country=GB&country=DE&protocol=https&use_mirror_status=on" | sed -e "s/^#Server/Server/" -e "/^#/d" | rankmirrors -n 5 -'
 
+# Git related
 alias gitpullrecursive='find . -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull" \;'
+alias gps='git push'
+alias gst='git status'
+alias gad='git add'
+alias gdf='git diff'
 alias gcm='git commit -m'
 
-alias cleanemptydir='sudo find . -type d -empty -delete' # remove empty directories
-alias cleancoderunner='find . -type f  -name "ft*" ! -name "*.c" -delete' # clean unused test compiled files from vscode coderunner extensions
+# 42 related
+alias clean_coderunner='find . -maxdepth 1 -type f -name "ft*" ! -name "*.c" -delete' # clean unused test compiled files from vscode coderunner extensions
+alias 42tester='/home/maplepy/Github/42/testers/francinette/tester.sh'
 
-alias pastehere='sleep 2; xdotool type "$(xclip -o -selection clipboard)"'
-
+# Systemctl
 alias sstart='sudo systemctl start'
 alias srstart='sudo systemctl restart'
 alias sstop='sudo systemctl stop'
 
-#
-## Config files
-#
+# Misc
+alias grub_regen='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias pastehere='sleep 2; xdotool type "$(xclip -o -selection clipboard)"'
+alias cleanemptydir='sudo find . -type d -empty -delete' # remove empty directories
+
+# Config files
 alias modalacritty='v ~/.config/alacritty/alacritty.yml'
 alias modbspwm='v ~/.config/bspwm/bspwmrc'
 alias moddocker='v /docker/docker-compose.yml'
@@ -60,17 +65,21 @@ alias fishreload='source ~/.config/fish/config.fish'
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # temporary
-alias 1280p='xrandr --output Virtual-1 --mode 1280x720 && bspc wm -r'
-alias 1024p='xrandr --output Virtual-1 --mode 1024x768 && bspc wm -r'
-alias 1920p='xrandr --output Virtual-1 --mode 1920x1080 && sleep 1 && bspc wm -r'
+# alias 1280p='xrandr --output Virtual-1 --mode 1280x720 && bspc wm -r'
+# alias 1024p='xrandr --output Virtual-1 --mode 1024x768 && bspc wm -r'
+# alias 1920p='xrandr --output Virtual-1 --mode 1920x1080 && sleep 1 && bspc wm -r'
 
 alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
+alias egrep='grep --color=auto -E'
+alias fgrep='grep --color=auto -F'
+
+#
+## EXPORTS
+#
 
 # export PATH='":$PATH
 export EDITOR='nvim'
 export SUDO_EDITOR='vim'
 export TERMINAL='alacritty'
 export MANPAGER='nvim +Man!'
-export MANWIDTH=999
+# export MANWIDTH=999
